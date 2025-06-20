fbd5296ce4a1169818e6f9e3e7ad9d5b146b8515
ruizdani301 <ruizdani301@gmail.com>
2025-06-19 22:08:30 -0500

    update agent, is necesary create sale done function

---
+++ pharma_vendor_chatbot_agent/all_tools/all_tools.py
@@ -0,0 +1,25 @@
+import json
+import os
+from langchain_core.tools import tool
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+
+os.environ["GOOGLE_API_KEY"] = "AIzaSyBlnimG4d7ptomNftBGYPgG3yqIEppWChA"
+@tool
+def search_medicine(text_request):
+    """
+    Search for a medicine requested.
+
+    Args:
+        text_request (str): The name of the medicine to search for.
+
+    Returns:
+        str: The description of the document containing the medicine found.
+    """
+    with open('medicaments.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    vectorizer_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", task_type="SEMANTIC_SIMILARITY")
+    vector_store = InMemoryVectorStore.from_texts(data, vectorizer_embeddings)
+    docs = vector_store.similarity_search(text_request, k=1)
+    for document in docs:
+        return document.page_content
--- pharma_vendor_chatbot_agent/main.py
+++ pharma_vendor_chatbot_agent/main.py
@@ -1,16 +1,19 @@
 import os
 import json
 import re
-import time
+from all_tools.all_tools import search_medicine
 from register_update import update_json
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_core.tools import tool
-from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
+
+tools = [search_medicine]
+model = init_chat_model("gemini-2.0-flash", model_provider="google_genai", temperature=0)
+memory = MemorySaver()
+models_with_tools = model.bind_tools([search_medicine])
 
 
 
@@ -20,71 +23,33 @@ def sale_done():
     #a update_json para actulizar el json
     pass
 
-@tool
-def search_medicine(text_request):
-    """
-    Search for a medicine requested.
 
-    Args:
-        text_request (str): The name of the medicine to search for.
-
-    Returns:
-        str: The description of the document containing the medicine found.
-    """
-    with open('medicaments.json', 'r', encoding='utf-8') as json_file:
-        data = json.load(json_file)
-    vectorizer_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", task_type="SEMANTIC_SIMILARITY")
-    vector_store = InMemoryVectorStore.from_texts(data, vectorizer_embeddings)
-    docs = vector_store.similarity_search(text_request, k=1)
-    for document in docs:
-        return document.page_content
-
-tools = [search_medicine]
-model = init_chat_model("gemini-2.0-flash", model_provider="google_genai", temperature=0)
-memory = MemorySaver()
-models_with_tools = model.bind_tools([search_medicine])
 def agent_chat(request_client):
-        messages = [
-        SystemMessage("Your are a pharmacist and seller of medicines try to sell medicines to the customers helping with information about medicines be clear, output max 50 characters"),
-        HumanMessage(request_client),
-    ]
+        template = ChatPromptTemplate([
+        ("system", "Your are a pharmacist and seller of medicines try to sell medicines to the customers helping with information about medicines be clear, output max 50 characters"),
+        ("human", f"{request_client}"),
+    ])
+
+        template_message = template.format(request_client=request_client)
 
         agent_executor = create_react_agent(model, tools, checkpointer=memory)
     
         config = {"configurable": {"thread_id": "abc123"}}
         for step in agent_executor.stream(
-            {"messages": messages}, config, stream_mode="values"
+            {"messages": template_message}, config, stream_mode="values"
         ):
             step["messages"][-1]
-            print(step["messages"][-1])
-            return step["messages"][-1]
+           # print(step["messages"][-1])
+        return step["messages"][-1]
 
 a = ""
 request_client = ""
 while request_client != "salir":
     request_client = input("What medicine are you looking for?")
     # configure basic variables
-   
     a = agent_chat(request_client)
-    
-    time.sleep(4)
-    print(a)
-    # def agent_chat(request_client):
-    #     messages = [
-    #     SystemMessage("Your are a pharmacist and seller of medicines try to sell medicines to the customers helping with information about medicines be clear, output max 50 characters"),
-    #     HumanMessage(request_client),
-    # ]
+    print(a.content)
 
-    #     agent_executor = create_react_agent(model, tools, checkpointer=memory)
-    
-    #     config = {"configurable": {"thread_id": "abc123"}}
-    #     for step in agent_executor.stream(
-    #         {"messages": messages}, config, stream_mode="values"
-    #     ):
-    #         step["messages"][-1]
-    #         print(step["messages"][-1])
-
-        
 
 
 
